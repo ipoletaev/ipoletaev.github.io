73503cd1d5171405bd6e79def6effe2501f68707
Igor Poletaev <poletaev.igor.phys@gmailc.com>
2019-07-29 23:39:10 -0700

    Fix disqus.js

--- js/disqus.js
+++ js/disqus.js
@@ -1,13 +1,13 @@
 var disqus_shortname = 'iepoletaev';
 (function() {
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://iepoletaev.disqus.com/embed.js';
+    dsq.src = 'https://iepoletaev.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
 
 (function () {
   var s = document.createElement('script'); s.async = true;
-  s.src = 'http://iepoletaev.disqus.com/count.js';
+  s.src = 'https://iepoletaev.disqus.com/count.js';
   (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
 
