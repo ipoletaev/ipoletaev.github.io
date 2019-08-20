05a657f9a378b0660cafdfedcf393eae437101f3
Ruslan Yurchenko <kort9591@gmail.com>
2019-08-20 21:17:10 +0300

    Fix tooltip on mobile, drop reverse anchor from content

--- js/footnotes.js
+++ js/footnotes.js
@@ -1,15 +1,27 @@
 jQuery(document).ready(function () {
   var footnotelinks = $('.footnoteRef');
-
   footnotelinks.each(function () {
     var $ref = $(this);
     var targetId = $ref.attr('href');
 
-    new Tooltip($ref, {
+    tippy(this, {
+      content: '',
+      animation: 'fade',
+      theme: 'custom',
+      distance: 0,
+      maxWidth: 400,
+      interactive: true,
+      interactiveBorder: 10,
       placement: 'bottom',
-      title: $(targetId).html(),
-      html: true,
-      template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>',
+      ignoreAttributes: true,
+      arrow: false,
+      onShow: function (instance) {
+        if (instance.popperChildren.content.textContent === '') {
+          var rawContent = $(targetId).html();
+          var cropIndex = rawContent.indexOf('<a href="#');
+          instance.setContent(rawContent.substring(0, cropIndex));
+        }
+      },
     });
   });
 });
